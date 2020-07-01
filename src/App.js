eb2cd086c98c9bbe1c7e6349ae9cfcd4fa7f036b
zhichao.feng <zhichao.feng@quantinfotech.com>
2020-07-01 13:34:00 +0800

    # test3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div id="myCanvas">
-                test111
+                test002
             </div>
         );
     }
