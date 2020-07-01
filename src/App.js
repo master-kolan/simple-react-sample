678997c72275bea216106b641e7dfc7ab38c46ef
zhichao.feng <zhichao.feng@quantinfotech.com>
2020-07-01 11:42:33 +0800

    # test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import InputNumber1 from "./InputNumber";
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <InputNumber1/>
+            <div id="myCanvas">
             </div>
         );
     }
